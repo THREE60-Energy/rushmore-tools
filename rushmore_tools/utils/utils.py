4ed876136848992f9f2e781461ffd30eb39d0431
Marcus Risanger <69350948+MarcusRisanger@users.noreply.github.com>
2022-03-22 15:48:55 +0100

    Added completion/drilling schemas (#3)
    Ready for pruning..

--- rushmore_tools/utils/utils.py
+++ rushmore_tools/utils/utils.py
@@ -1,4 +1,10 @@
+import json
+
+
 def read_test_data(report_name: str):
     with open(f"test_data//{report_name.lower()}_dump.txt") as f:
         data = f.read()
-    return list(eval(data))
+    try:
+        return list(eval(data))
+    except:
+        return json.loads(data)
